6741b8918feca95038e797764073bf19ddb7defd
yunwang <yunwang@ebay.com>
2020-04-01 22:14:23 +0800

    update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -2,4 +2,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-../vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" k8s.io/cgtest/generated k8s.io/cgtest/api cgcontroller:v1alpha1 --go-header-file $(pwd)/boilerplate.go.txt --output-base $(pwd)/../../
+../vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" github.com/YunWang/cgtest/generated github.com/YunWang/cgtest/api cgcontroller:v1alpha1 --go-header-file $(pwd)/boilerplate.go.txt --output-base $(pwd)/../../
